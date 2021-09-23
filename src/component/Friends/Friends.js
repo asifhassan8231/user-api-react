e94d3c49f8801786f65b964e633c44494924e8ed
asifhassan8231 <asifhassan8231@gmail.com>
2021-09-23 12:15:24 +0600

    final commit

--- src/component/Friends/Friends.js
+++ src/component/Friends/Friends.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Friends.css'
 
 const Friends = () => {
-    const emailList = localStorage.getItem("email_List");
-    const emailParsed = JSON.parse(emailList);
-    const myObj = Object.keys(emailParsed);
+    // const emailList = localStorage.getItem("email_List");
+    // const emailParsed = JSON.parse(emailList);
+    // const myObj = Object.keys(emailParsed);
     return (
         <div className="friends">
-            <h2>Friends email <span>({myObj.length})</span></h2>
+            {/* <h2>Friends email <span>({myObj.length})</span></h2>
             {
                 myObj.map(obj => <li>{obj}</li>)
-            }
+            } */}
         </div>
     );
 };
